615c989281ee444d51606089bcefdef18b47290f
Bhanu Murari Sankarasetty <168958411+BhanuSankarasetty@users.noreply.github.com>
2025-08-15 23:50:11 +0530

    Initail commit

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Image from 'next/image'  
+import { cn } from '@/lib/utils'
+import clientPromise from '@/lib/mongodb'
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
+import { getServerSession } from 'next-auth/next'
+import { Toaster } from '@/components/ui/toaster'
+import { ThemeProvider } from '@/components/theme-provider'
+
+
+export default function Home() {
+  return (
+      <ThemeProvider theme={theme}>
+        <Button>This button has disabled ripples.</Button>
+      </ThemeProvider>
+  );
+}
